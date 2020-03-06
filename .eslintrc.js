b1f086566de7c9a96f1441e1adf07cceb58e72fd
Matteo Vivona <matteo.vivona@me.com>
2020-03-06 15:48:59 +0100

    test(eslintrc): create eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  ignorePatterns: ["commitlint.config.js"],
+  env: {
+    browser: true,
+    es6: true,
+    "jest/globals": true
+  },
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: "./"
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
+  plugins: ["react", "@typescript-eslint", "prettier", "jest"],
+  rules: {
+    "@typescript-eslint/member-delimiter-style": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/unified-signatures": "error",
+    "@typescript-eslint/no-inferrable-types": [
+      "error",
+      { ignoreParameters: true }
+    ],
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      { allow: ["arrowFunctions"] }
+    ],
+    "import/extensions": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-wrap-multilines": "off",
+    "react/jsx-props-no-spreading": "off",
+    "arrow-body-style": ["warn", "as-needed"],
+    "no-param-reassign": ["error", { props: false }],
+    "import/prefer-default-export": "off",
+    "no-console": "off",
+    "eol-last": ["error", "always"],
+    "no-debugger": "error",
+    "no-nested-ternary": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": ["error", "never"],
+    curly: ["error", "all"]
+  }
+};
