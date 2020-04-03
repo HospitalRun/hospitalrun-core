562a46e6554a4e301d3f7fc968fda551aeb57ebc
Maksim Sinik <maksim@sinik.it>
2020-04-03 19:12:47 +0200

    fix(model): remove unused models

--- src/model/elements/created-at.ts
+++
@@ -1,7 +0,0 @@
-import { Static } from '@sinclair/typebox'
-
-import { DateTimeSchema } from '../primitives/date-time'
-
-export const CreatedAtSchema = DateTimeSchema
-
-export type CreatedAt = Static<typeof CreatedAtSchema>
