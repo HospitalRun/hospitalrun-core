c83cd66e188a9f1880346678fa2bbe6b5ad52b7b
Jack Meyer <jackcmeyer@gmail.com>
2020-05-04 20:54:43 -0500

    feat(appointment): add appointment schema

---
+++ src/model/elements/appointment-type.ts
@@ -0,0 +1,13 @@
+import { Type, Static } from '@sinclair/typebox'
+
+export const AppointmentTypeSchema = Type.Union(
+  [Type.Literal('checkup'),
+    Type.Literal('in-patient'),
+    Type.Literal('emergency'),
+    Type.Literal('follow up'),
+    Type.Literal('routine'),
+    Type.Literal('walk in'),
+  ],
+)
+
+export type PatientType = Static<typeof AppointmentTypeSchema>
